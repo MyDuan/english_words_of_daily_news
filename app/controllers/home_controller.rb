07a7f13a707d5f83ee0c509b35f9faad51d1caa4
MyDuan <dy1226130575@163.com>
2019-09-28 23:15:23 +0900

    Add flash notice.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,6 @@ class HomeController < ApplicationController
   end
 
   def search
-
     if params[:title] != ''
       words_in_article = Article.where(title: params[:title])
     elsif params[:word] != ''
@@ -12,15 +11,19 @@ class HomeController < ApplicationController
     elsif params[:date] != ''
       date = params[:date].split('-')
       words_in_article = Article.where(release_year: date[0], release_month: date[1], release_day: date[2])
+    else
+      flash[:notice] = "No search conditions!"
+      redirect_to("/") and return
     end
 
-    if words_in_article
+    if words_in_article[0]
       year = words_in_article[0].release_year
       month = words_in_article[0].release_month
       day = words_in_article[0].release_day
       redirect_to("/article/show/#{year}/#{month}/#{day}")
     else
       flash[:notice] = "No results!"
+      redirect_to("/") and return
     end
 
   end
@@ -29,6 +32,9 @@ class HomeController < ApplicationController
     if params[:year] != '' and params[:month] != ''
       words_in_article = Article.where(release_year: params[:year], release_month: params[:month])
       redirect_to("/article/show_one_month/#{params[:year]}/#{params[:month]}")
+    else
+      flash[:notice] = "No search conditions!"
+      redirect_to("/")
     end
   end
 
