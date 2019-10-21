e4ea08b8ba5b7ad1fdff055b90210035cd9fe031
MyDuan <dy1226130575@163.com>
2019-10-21 10:11:28 +0900

    Change sqlite3 to pgsql.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_09_28_032759) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.text "title"
     t.integer "release_year"
